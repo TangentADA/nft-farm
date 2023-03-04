13aef3b7140a1acd211a83d84c34174b48e98b7c
Clint Alexander <100855173+GavinKarlsson@users.noreply.github.com>
2023-03-04 06:07:51 +0400

    Staking Plutus V.0.1

---
+++ app/utils-stake-key-hash.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import System.Environment (getArgs)
+import qualified Utils (unsafeReadAddress, unsafeStakePubKeyHash)
+
+--Modulo:
+
+main :: IO ()
+main = do
+  [addr'] <- getArgs
+  print $ Utils.unsafeStakePubKeyHash $ Utils.unsafeReadAddress addr'
